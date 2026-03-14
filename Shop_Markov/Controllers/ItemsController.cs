fdd6587725d7688c20b35b6d906c5c8109d8b960
DikiYLexa <dubovan36@gmail.com>
2026-03-14 15:13:51 +0500

    Разработанный контроллер предметов. markov

---
+++ Shop_Markov/Controllers/ItemsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop_Markov.Data.Interfaces;
+
+namespace Shop_Markov.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private IItems IAllItems;
+        private ICategories IAllCategories;
+
+        public ItemsController(IItems iAllItems, ICategories iAllCategories)
+        {
+            this.IAllItems = IAllItems;
+            this.IAllCategories = IAllCategories;
+        }
+
+        public ViewResult List()
+        {
+            ViewBag.Title = "Страница с предметами";
+
+            var cars = IAllItems.AllItems;
+            return View(cars);
+        }
+    }
+}
--- Shop_Markov/Program.cs
+++ Shop_Markov/Program.cs
@@ -1,3 +1,6 @@
+using Shop_Markov.Data.Interfaces;
+using Shop_Markov.Data.Mocks;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMvc(option => option.EnableEndpointRouting = false);
 var app = builder.Build();
@@ -7,3 +10,11 @@ app.UseStatusCodePages();
 app.UseStaticFiles();
 app.UseMvcWithDefaultRoute();
 app.Run();
+
+public void ConfigureServices(IServiceCollection services)
+{
+    services.AddTransient<ICategories, MockCategories>();
+    services.AddTransient<IItems, MockItmes>();
+
+    services.AddMvc(option => option.EnableEndpointRouting = false);
+}

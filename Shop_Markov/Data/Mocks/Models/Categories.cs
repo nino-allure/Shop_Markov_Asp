8ccb06407ea7eb6a56d3aaaafb9f9eccf7b67bed
DikiYLexa <dubovan36@gmail.com>
2026-03-14 14:50:46 +0500

    Разработанные модели данных.  markov

---
+++ Shop_Markov/Data/Mocks/Models/Categories.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+namespace Shop_Markov.Data.Mocks.Models
+{
+    public class Categories
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<items> Items { get; set; }
+    }
+}
---
+++ Shop_Markov/Data/Mocks/Models/Items.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+namespace Shop_Markov.Data.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Img { get; set; }
+        public int Price { get; set; }
+        public Categories Category { get; set; }
+    }
+}

478b14a63db9e63145278d4879674743c6d890ab
DikiYLexa <dubovan36@gmail.com>
2026-03-21 13:30:31 +0500

    Получение данных о категориях. markov

---
+++ Shop_Markov/Data/DataBase/DBCategory.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using Shop_Markov.Common;
+using Shop_Markov.Data.Interfaces;
+using Shop_Markov.Data.Mocks.Models;
+using Shop_Markov.Data.Models;
+using System.Collections.Generic;
+
+
+namespace Shop_Markov.Data.DataBase
+{
+    public class DBCategory : ICategories
+    {
+        public IEnumerable<Categories> AllCategories
+        {
+            get
+            {
+                List<Categories> categories = new List<Categories>();
+                MySqlConnection MySqlConnection = Connection.MySqlOpen();
+                MySqlDataReader CategoriesData = Connection.MySqlQuery("SELECT * FROM Shop.Categories ORDER BY `Name`;", MySqlConnection);
+                while (CategoriesData.Read())
+                {
+                    categories.Add(new Categories()
+                    {
+                        Id = CategoriesData.IsDBNull(0) ? -1 : CategoriesData.GetInt32(0),
+                        Name = CategoriesData.IsDBNull(1) ? null : CategoriesData.GetString(1),
+                        Description = CategoriesData.IsDBNull(2) ? null : CategoriesData.GetString(2)
+                    });
+                }
+                return categories;
+            }
+        }
+    }
+}

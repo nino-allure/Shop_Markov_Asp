702db22bbd850723fdf893cf022e4b4c1bc451c7
DikiYLexa <dubovan36@gmail.com>
2026-03-21 13:18:02 +0500

    Разработанное подключение к MySql. markov

---
+++ Shop_Markov/Common/Connection.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1.Mozilla;
+
+namespace Shop_Markov.Common
+{
+    public class Connection
+    {
+        readonly static string ConnectionData = "Server=localhost;Database=Shop;Uid=root;Pwd=;";
+
+        public MySqlConnection MySqlOpen()
+        {
+            MySqlConnection NewMySqlConnection = new MySqlConnection(ConnectionData);
+            NewMySqlConnection.Open();
+
+            return NewMySqlConnection;
+        }
+
+        public static MySqlDataReader MySqlQuery(string Query, MySqlConnection Connection)
+        {
+            MySqlCommand NewMySqlCommand = new MySqlCommand(Query, Connection);
+            return NewMySqlCommand.ExecuteReader();
+        }
+
+        public static void MySqlClose(MySqlConnection connection)
+        {
+            connection.Close();
+        }
+    }
+}

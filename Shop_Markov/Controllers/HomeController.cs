b9465a5240fbf795f991dd570d35e9922e3d5520
DikiYLexa <dubovan36@gmail.com>
2026-03-28 13:39:20 +0500

    Разработанное перенаправление на страницу с предметами. markov

---
+++ Shop_Markov/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop_Markov.Controllers
+{
+    public class HomeController : Controller
+    {
+        public RedirectResult Index()
+        {
+            return Redirect("/Items/List");
+        }
+    }
+}
